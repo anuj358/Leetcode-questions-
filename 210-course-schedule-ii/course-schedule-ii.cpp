a1658d7ed34b42f7e8d6a7523153b7b9ad82897d
anuj358 <kanhasharmaji2004@gmail.com>
2026-02-26 22:43:46 +0530

    Time: 0 ms (100.00%) | Memory: 18.4 MB (58.19%) - LeetSync

---
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        // adjacency list
+        vector<vector<int>> adj(numCourses);
+        vector<int> indegree(numCourses, 0);
+
+        // build graph
+        for(int i = 0; i < prerequisites.size(); i++){
+            int course = prerequisites[i][0];
+            int prereq = prerequisites[i][1];
+
+            adj[prereq].push_back(course);
+            indegree[course]++;
+        }
+
+        // Kahn's Algorithm
+        queue<int> q;
+
+        for(int i = 0; i < numCourses; i++){
+            if(indegree[i] == 0)
+                q.push(i);
+        }
+
+        int count = 0;
+        vector<int>ans;
+
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            ans.push_back(node);
+
+            for(int i = 0; i < adj[node].size(); i++){
+                indegree[adj[node][i]]--;
+
+                if(indegree[adj[node][i]] == 0)
+                    q.push(adj[node][i]);
+            }
+        }
+        vector<int>temp;
+
+
+        return ans.size()==numCourses?ans:temp ;
+    }
+};
