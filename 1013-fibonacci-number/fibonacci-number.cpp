3236757142642d9f76bd75a85bc3b200f52cc105
anuj358 <kanhasharmaji2004@gmail.com>
2026-04-03 00:22:10 +0530

    Time: 3 ms (65.29%) | Memory: 8.3 MB (10.59%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+
+    int fibon(int n,vector<int>&dp){
+        if(n<=1)
+        return n;
+
+        if(dp[n]!=-1)
+        return dp[n];
+
+        return dp[n] = fibon(n-1,dp)+fibon(n-2,dp);
+    }
+
+    int fib(int n) {
+        vector<int>dp(n+1,-1);
+        return fibon(n,dp);
+    }
+};
